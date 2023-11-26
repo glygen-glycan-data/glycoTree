6416a4a60e26bb4986b8ca40ed1c98e7be68b08a
Nathan Edwards <edwardsnj@gmail.com>
2023-11-26 14:12:05 -0500

    ensure extra_[NO] are created

--- data/dl.sh
+++ data/dl.sh
@@ -15,10 +15,12 @@ $GGACCS GlyGen allglycans > glygen_allacc.txt
 rm -rf input_N
 mkdir -p input_N
 $GCTCONID N-linked input_N
+mkdir -p extra_N
 
 #O-linked
 
 rm -rf input_O
 mkdir -p input_O
 $GCTCONID O-linked input_O
+mkdir -p extra_O
 
