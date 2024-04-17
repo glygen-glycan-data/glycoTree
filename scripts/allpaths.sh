a07a20aee517c974a7b2f3820ea596028cbba3e9
Nathan Edwards <edwardsnj@gmail.com>
2024-04-17 10:18:20 -0400

    updates to path logic

--- scripts/allpaths.sh
+++ scripts/allpaths.sh
@@ -8,5 +8,5 @@ ls -l
 . ./bashrc
 cd ..
 # start php-mysql-apache
-docker-compose exec web apt-get install jq wget
+docker-compose exec web apt-get install -y jq wget
 docker-compose exec -u `id -u`:`id -g` web sh -c "cd /glycoTree/portal/api/paths; wget -q -O - 'http://localhost/api/list.php?mode=mapped' | jq -r '.[].glytoucan_ac' > mappedglycans.txt; ./genAllPaths.sh mappedglycans.txt allPaths.json"
