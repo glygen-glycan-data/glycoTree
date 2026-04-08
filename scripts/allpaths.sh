10129cf11736cc491d6b7181a13f5ecb07a64c1a
Nathan Edwards <edwardsnj@gmail.com>
2026-04-08 11:30:19 -0400

    Make allpaths.json compressed for github

--- scripts/allpaths.sh
+++ scripts/allpaths.sh
@@ -10,3 +10,4 @@ cd ..
 # start php-mysql-apache
 docker-compose exec web apt-get install -y jq wget
 docker-compose exec -u `id -u`:`id -g` web sh -c "cd /glycoTree/portal/api/paths; wget -q -O - 'http://localhost/api/list.php?mode=mapped' | jq -r '.[].glytoucan_ac' > mappedglycans.txt; ./genAllPaths.sh mappedglycans.txt allPaths.json"
+gzip -9 portal/api/paths/allPaths.json
